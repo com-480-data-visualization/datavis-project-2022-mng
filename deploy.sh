76150274d7b440874bd481da3c8fc9b8170b1ef4
ogimgio <ogimgamers@gmail.com>
2022-04-22 13:36:22 +0200

    small fix

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:ogimgio/datavis-project-2022-mng.git master:gh-pages
+git push -f git@github.com:com-480-data-visualization/datavis-project-2022-mng.git master:gh-pages
 
 cd -
