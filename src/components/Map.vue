98aaa3435bd112fff7dcce13cc1e9ccc4fd7c029
Nicky <nicolas.mario.baldwin@gmail.com>
2022-05-04 13:56:40 +0200

    fixed zoom

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
   <h2 v-if="province" class="province-title">{{province.name}}</h2>
-    <h2 v-if="currentProvince" class="province-title">{{currentProvince.name}}</h2>
     <div class="center-screen" id='chart' ></div>
   </div>
  <!-- <svg-map :map="Taiwan"></svg-map> -->
@@ -24,8 +23,8 @@ export default {
       Taiwan,
       geoJsonObj: json,
       communes: json2,
-      province: undefined,
-      currentProvince: undefined,
+      province: 'non definito',
+      currentProvince: 'non definito',
     };
   },
   created() {
@@ -76,8 +75,8 @@ export default {
 
 
       const svg = d3.select("#chart").append("svg")
-          .attr('width',1000)
-          .attr('height',1000);
+          .attr('width',width)
+          .attr('height',height);
       const that = this;
 
       svg.append("g")
@@ -90,32 +89,143 @@ export default {
           .attr("d", path)
           .on("mouseover",function (e,d){
             // gives me the correct values
-            console.log(d.properties)
+            //console.log(d.properties)
+            console.log(d)
             d3.select(this).style('fill', '#1483ce');
-            that.selectProvince(d.properties)
+            that.selectProvince('non definito');
+            //that.selectProvince(d.properties)
           })
           .on('mouseout', function () {
-            that.selectProvince(undefined);
+            that.selectProvince('non definito');
             // Reset province color
             svg.selectAll('path')
                 .style('fill', (d) => {
-                  console.log("ciao")
-                  console.log(d)
-                  console.log(centered)
+                  //console.log("ciao")
+                  //console.log(d)
+                  //console.log(centered)
                   return centered && d===centered ? '#D5708B' : fillFn(d);
                 });
           })
           .on('click', clicked);
+
+      function getMaxCoordinatesFromBorders(d){
+        const x_coord = 0
+        const y_coord = 1
+        var max_x_coord =  d.geometry.coordinates
+                                .map((borders)=> 
+                                                  Math.max.apply(
+                                                      Math,
+                                                      borders
+                                                        .map((border_coordinate) => 
+                                                                Math.max.apply(
+                                                                          Math,
+                                                                          border_coordinate.map(coordinate => coordinate[x_coord])
+                                                                )
+                                                            )
+                                                  )
+                                      )
+        var max_y_coord =  d.geometry.coordinates
+                                .map((borders)=> 
+                                                  Math.max.apply(
+                                                      Math,
+                                                      borders
+                                                        .map((border_coordinate) => 
+                                                                Math.max.apply(
+                                                                          Math,
+                                                                          border_coordinate.map(coordinate => coordinate[y_coord])
+                                                                )
+                                                            )
+                                                  )
+                                      )
+        return {max_x_coord,max_y_coord}
+      }
+
+      function getMinCoordinatesFromBorders(d){
+        const x_coord = 0
+        const y_coord = 1
+        var min_x_coord =  d.geometry.coordinates
+                                .map((borders)=> 
+                                                  Math.min.apply(
+                                                      Math,
+                                                      borders
+                                                        .map((border_coordinate) => 
+                                                              Math.min.apply(
+                                                                        Math,
+                                                                        border_coordinate.map(coordinate => coordinate[x_coord])
+                                                              )
+                                                            )
+                                                    )
+                                      )
+        var min_y_coord =  d.geometry.coordinates
+                                .map((borders)=> 
+                                                  Math.min.apply(
+                                                      Math,
+                                                      borders
+                                                        .map((border_coordinate) => 
+                                                                Math.min.apply(
+                                                                          Math,
+                                                                          border_coordinate.map(coordinate => coordinate[y_coord])
+                                                                )
+                                                            )
+                                                  )
+                                      )
+        
+        return {min_x_coord,min_y_coord}
+      }
+
+      function getBoundingBox(d){
+        console.log('entered_bbox')
+        
+        let {max_x_coord,max_y_coord} = getMaxCoordinatesFromBorders(d)
+        let {min_x_coord,min_y_coord} = getMinCoordinatesFromBorders(d)
+        console.log('min_x_coord ' + min_x_coord)
+        console.log('min_y_coord ' +  min_y_coord)
+        console.log('max_x_coord ' + max_x_coord)
+        console.log('max_y_coord ' +max_y_coord)
+        
+        console.log(minX)
+        console.log(minY)
+        console.log(maxX)
+        console.log(maxY)
+
+        max_x_coord = x(max_x_coord) // rescale coordinates
+        max_y_coord = y(max_y_coord)// rescale coordinates
+        min_x_coord = x(min_x_coord)// rescale coordinates
+        min_y_coord = y(min_y_coord)// rescale coordinates
+
+        console.log('min_x_coord ' + min_x_coord)
+        console.log('min_y_coord ' +  min_y_coord)
+        console.log('max_x_coord ' + max_x_coord)
+        console.log('max_y_coord ' +max_y_coord)
+        //var max_x = Math.max(d.geometry.coordinates.forEach((prop)=> Math.min(prop.map((x) => x.map(y => y[0])))));
+        //var max_y = Math.max(d.geometry.coordinates.forEach((prop)=> Math.min(prop.map((x) => x.map(y => y[1])))));
+        //var min_x = Math.min(d.geometry.coordinates.forEach((prop)=> Math.min(prop.map((x) => x.map(y => y[0])))));
+        //var min_y = Math.min(d.geometry.coordinates.forEach((prop)=> Math.min(prop.map((x) => x.map(y => y[1])))));
+        //console.log('max: ' + max_x + ',' + max_y)
+        //console.log('min: ' + min_x + ',' + min_y)
+        return {max_x_coord,max_y_coord,min_x_coord,min_y_coord}
+      }
+
+
       function clicked(e,d) {
         var x, y, k;
-
+        
+        let {max_x_coord,max_y_coord,min_x_coord,min_y_coord} = getBoundingBox(d)
+        
+        console.log(bbox)
         // Compute centroid of the selected path
         if (d && centered !== d) {
           //TODO: fix this
           var centroid = path.centroid(d);
           x = centroid[0];
           y = centroid[1];
-          k = 4;
+          console.log(Math.abs(max_x_coord - min_x_coord))
+          console.log(Math.abs(max_y_coord - min_y_coord))
+          var scale_x = Math.ceil(size.width / Math.abs(max_x_coord - min_x_coord))
+          var scale_y = Math.ceil(size.height / Math.abs(max_y_coord - min_y_coord))
+          
+          k =  scale_x < scale_y ? scale_x : scale_y;
+    
           centered = d;
           that.openInfo(d.properties);
         } else {
@@ -126,6 +236,9 @@ export default {
           that.closeInfo();
         }
 
+        console.log('x: '+ x)
+        console.log('y: '+ y)
+
         // Highlight the clicked province
         svg.append('g').selectAll('path')
             .style('fill', function(d){
@@ -133,21 +246,24 @@ export default {
             });
         // Zoom
         //TODO: needs to be fixed
-        if(!zoomed) {
-          svg.transition()
-              .duration(750)
-              .attr('transform', 'translate(' + size.width / 2 + ',' + size.height / 2 + ')scale(' + k + ')translate(' + -x + ',' + -y + ')');
-
-          zoomed = true
-        }
+        //if(!zoomed) {
+          
+        svg.transition()
+            .duration(750)
+            .attr('transform', 'scale(' + k + ')' + 'translate(' + size.width / 2 + ',' + size.height / 2  +')translate(' + -x + ',' + -y + ')' );
+          
+        zoomed = true
+        console.log(zoomed)
+        //}
+        /*
         else{
           svg.transition()
               .duration(750)
               //undefined allows for faster zoomed out.
-              .attr('transform',0)
+              .attr('transform',undefined)
           zoomed = false
         }
-
+*/
       }
 
 // Get province name length
