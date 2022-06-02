f29312d8d6e618b1e1a2061385a379cfe3bf7026
ogimgio <ogimgamers@gmail.com>
2022-06-02 14:41:04 +0200

    rep

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <template>
-  <div style=" text-align: justify;">
+  <div id="introduction" style=" text-align: justify;">
     <div style="margin-top:50px;margin-right: auto;
     margin-left: auto;padding: 50px 15px 75px;max-width: 850px;">
-      <h1 id="introduction" style="font-size: 50px; text-align: center;color: #3b6b3b">
-        The interactive map of Switzerland</h1><br><br>
+      <h1 style="font-size: 50px; text-align: center;color: #3b6b3b;text-transform: uppercase">
+        The interactive map of Switzerland</h1><br><br><br>
       INTRODUCTIONOOOOOOOOOOOOOODUHFLSKGAUGUHLSGDULDGFUFLKSDGJUERLJKBSDGHIRUEOWLKHJSDFGIUSERJOWHKLDFGHIOUEHJRWKLBHLSGFDIJHKSBER,JKGUDFHXULKJSBREWLS<br><br>
     </div>
   </div>
